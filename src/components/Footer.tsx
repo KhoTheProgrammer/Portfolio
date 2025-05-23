101d570ae943b6c758f28152f9cd3d5f4ea48616
kondwani-padyera <kondwanipadyera@gmail.com>
2025-05-23 18:13:47 +0200

    Implemented the footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-[300x] p-4 bg-[#040b14] fixed left-0 bottom-0 z-[9997]">
+      <p className=" text-white">Copyright <strong>Kondwani Padyera</strong> 2025</p>
+    </div>
+  )
+}
+
+export default Footer
