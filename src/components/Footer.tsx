73f4b0dc9548c881347b62b04843448d9274863a
kondwani-padyera <kondwanipadyera@gmail.com>
2025-05-24 15:51:22 +0200

    `Refactor Sidebar component to accept isOpen and toggleSidebar props, add dynamic navigation links and mobile sidebar toggle functionality.`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="w-[300x] p-4 bg-[#040b14] fixed left-0 bottom-0 z-[9997]">
-      <p className=" text-white">Copyright <strong>Kondwani Padyera</strong> 2025</p>
+    <div className="lg:w-[300px] p-4 bg-[#040b14] lg:fixed lg:left-0 lg:bottom-0 lg:z-[9997] w-full fixed bottom-0 text-white" onClick={() => console.log("pressed")}>
+      <p className="text-center" onClick={() => console.log("pressed")}>Copyright <strong>Kondwani Padyera</strong> 2025</p>
     </div>
   )
 }
